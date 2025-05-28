66abf58735206ad94f4631b1378266ae7681a491
IsraaMohamedGaber <mohamedisraa23@gmail.com>
2025-05-28 17:20:50 +0300

    update main to get

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ Instrumentator().instrument(app).expose(app)
 
 # Load model
 model = joblib.load("models/best_model.pkl")
+
 # Model-related: track prediction count
 model_prediction_total = Counter("model_prediction_total", "Total predictions made")
 
@@ -22,7 +23,12 @@ invalid_input_total = Counter("invalid_input_total", "Total invalid inputs recei
 class LandmarkInput(BaseModel):
     landmarks: List[float]  # Accept any list of floats
 
-@app.post("/predict/")
+# Root route with a welcome message
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Gesture Prediction API!"}
+
+@app.post("/predict")
 def predict(input_data: LandmarkInput):
     try:
         data = np.array(input_data.landmarks).reshape(1, -1)
